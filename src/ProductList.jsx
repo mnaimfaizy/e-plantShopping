162762a9ce68dbdb175843b98f9544e72173b388
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2024-07-18 23:28:32 -0400

    chore: update cartItem and cartTotal on product list

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -9,6 +9,7 @@ function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const [totalQuantityCounter, setTotalQuantityCounter] = useState(0);
     
     const plantsArray = [
         {
@@ -259,6 +260,11 @@ const handlePlantsClick = (e) => {
     }));
     };
 
+    useEffect(() => {
+        const total = Object.keys(addedToCart).length;
+        setTotalQuantityCounter(total);
+    }, [addedToCart]);
+
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -276,7 +282,23 @@ const handlePlantsClick = (e) => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                    <h1 className='cart'>
+                        <span>{totalQuantityCounter ?? 0}</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                            viewBox="0 0 256 256" 
+                            id="IconChangeColor" 
+                            height="68" 
+                            width="68">
+                                <rect width="156" height="156" fill="none"></rect>
+                                <circle cx="80" cy="216" r="12"></circle>
+                                <circle cx="184" cy="216" r="12"></circle>
+                                <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute">
+                                </path>
+                        </svg>
+                    </h1>
+                    </a>
+                </div>
             </div>
         </div>
         {!showCart? (
